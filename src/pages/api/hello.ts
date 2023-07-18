be65db3df711feff8832b21fa9b2cf6f44252b45
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-07-17 21:52:02 -0300

    Update hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,5 +2,5 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.query);
-  res.status(200).json({ name: 'Hello folks', params: { req.query } })
+  res.status(200).json({ name: 'Hello folks', params: req.query })
 }
