ec6976bb057b17ddb599b61db11d127a190e7124
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-07-17 22:22:33 -0300

    feat(Api routes): Added cron job (easy cron)

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,32 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { api } from '../../config/api';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.query);
+interface SendTextParams {
+  phone: string,
+  message: string
+}
+
+const listOfCellphoneNumber = [
+  process.env.CEL1,
+  process.env.CEL2,
+]
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    for (let i = 0; i < listOfCellphoneNumber.length; i++) {
+      const params: SendTextParams = {
+        message: `Pipoca!!!${i}`,
+        phone: listOfCellphoneNumber[i],
+      }
+
+      await api.post("send-text", params);
+    }
+  } catch (error) {
+    await api.post("send-text", {
+      message: "Deu erro mano",
+      phone: process.env.CEL2,
+    } as SendTextParams)
+  }
+
   res.status(200).json({ name: 'Hello folks', params: req.query })
 }
